e740746694bb9cb59d54f9a8be7aa1e4a1111124
itsmanjeet <itsmanjeet1998@gmail.com>
2020-04-06 23:38:29 +0530

    fixed request.get() to download file and added error handling for connection error

--- src/sys-update.py
+++ src/sys-update.py
@@ -8,10 +8,14 @@ app.addAuthor('Pratyush Ratan', email='ratanpratyush@releax.in')
 
 @app.args('check','[release]','check latest release')
 def check_updates(*args):
-    print("args",args)
     release = args[0].value_of('release', 'stable')
-    mirror = args[0].value_of('mirror', 'https://127.0.0.1:5000/')
+    mirror = args[0].value_of('mirror', 'http://127.0.0.1:5000/')
     url = "%s/check/%s" %  (mirror,release)
-    resp = requests.get(url)
+    try:
+        resp = requests.get(url)
+    except requests.exceptions.ConnectionError as e:
+        print("unable to connect to internet")
+        
     print(resp.json())
+
 app.run(sys.argv)
